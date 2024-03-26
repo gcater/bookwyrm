eb94c8def5442c7a3fd9a9a7f84f52ad6251c81b
gcater1 <geoffcater123@gmail.com>
2024-03-26 16:44:26 -0400

    adding shadcn compnonents. Mainly form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
+"use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
 import { api } from "~/utils/api";
+import { z } from "zod";
+import {
+  useForm,
+  FieldValues,
+  UseFormRegister,
+  ControllerFieldState,
+} from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface Book {
   id: number;
@@ -25,7 +46,8 @@ export default function Home() {
             <AuthShowcase />
           </div>
           <div>{uploadBook()}</div>
-          <div className="text-white">{bookDisplay()}</div>
+          {/* <div className="text-white">{bookDisplay()}</div> */}
+          {ProfileForm()}
         </div>
       </main>
     </>
@@ -117,6 +139,7 @@ function uploadBook() {
       >
         {submitting ? "Adding..." : "Add Book"}
       </button>
+      <div className="text-white">{bookDisplay()}</div>
     </form>
   );
 }
@@ -137,3 +160,60 @@ function bookDisplay() {
     </div>
   );
 }
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export function ProfileForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+// function zodResolver(
+//   formSchema: z.ZodObject<
+//     { username: z.ZodString },
+//     "strip",
+//     z.ZodTypeAny,
+//     { username: string },
+//     { username: string }
+//   >,
+// ): import("react-hook-form").Resolver<{ username: string }, any> | undefined {
+//   throw new Error("Function not implemented.");
+// }
