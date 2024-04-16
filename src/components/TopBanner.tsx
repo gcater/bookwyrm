2eed35d8d019c6a19cec7a62c8333d4de3aeab84
gcater1 <geoffcater123@gmail.com>
2024-04-16 15:32:01 -0400

    working on adding a menu popup

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { LucideProps } from "lucide-react";
+
 const TopBanner = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -10,30 +10,45 @@ const TopBanner = () => {
       <header className="flex w-full flex-col border-b border-gray-300 bg-white px-4 py-2">
         <h1 className="text-2xl font-bold">BookWyrm</h1>
 
-        <div className="dropdownToggleBtn  absolute right-0">
-          <div className="px-4">
-            <div className="svg-container">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="45"
-                height="45"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width=".5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-circle-ellipsis"
-              >
-                <circle cx="12" cy="12" r="10" stroke="#D1D5DB" />
-                <path d="M15 12h.01" stroke-width="1.5" />
-                <path d="M12 12h.01" stroke-width="1.5" />
-                <path d="M9 12h.01" stroke-width="1.5" />
-              </svg>
-            </div>
+        <div className="absolute right-0">
+          <div
+            className="svg-container cursor-pointer" // Ensure this class is here
+            onClick={toggleDropdown}
+            style={{ display: "inline-block", lineHeight: 0 }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="45"
+              height="45"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth=".5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-circle-ellipsis"
+              onClick={toggleDropdown}
+            >
+              {/* Apply the CSS variable to the fill attribute */}
+              <circle
+                cx="12"
+                cy="12"
+                r="10"
+                fill="var(--circle-fill-color)"
+                stroke="#D1D5DB"
+              />
+              <path d="M15 12h.01" strokeWidth="1.5" />
+              <path d="M12 12h.01" strokeWidth="1.5" />
+              <path d="M9 12h.01" strokeWidth="1.5" />
+            </svg>
           </div>
         </div>
       </header>
+      <div
+        className={`fixed right-4 top-[calc(3rem+12px)] flex items-center ${isDropdownOpen ? "" : "hidden"}`}
+      >
+        <div className="box-content h-32 w-48 max-w-xs rounded-md border border-gray-400 bg-white p-4 shadow-lg"></div>
+      </div>
     </div>
   );
 };
